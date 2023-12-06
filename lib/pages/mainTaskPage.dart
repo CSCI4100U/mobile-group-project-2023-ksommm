077d986d41ed3e29e74718a8b26c0b2b1024aa6f
mmuidev <matthewmui22@gmail.com>
2023-12-06 15:12:37 -0500

    code to test tasks, wip

--- lib/pages/mainTaskPage.dart
+++ lib/pages/mainTaskPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:main/pages/taskStart.dart';
+import 'package:main/storage/firestore_service.dart';
 
 import 'Task.dart';
 import 'TaskForm.dart';
@@ -8,15 +9,15 @@ import 'TaskModel.dart';
 
 class mainTaskPage extends StatefulWidget {
   final String? title;
+
   const mainTaskPage({super.key, this.title});
 
   @override
   State<mainTaskPage> createState() => _mainTaskPageState();
 }
 
-
 class _mainTaskPageState extends State<mainTaskPage> {
-  List listTask =[];
+  List listTask = [];
   int id_counter = 0;
   int counter = 0;
   var _task = TasksModel();
@@ -28,80 +29,81 @@ class _mainTaskPageState extends State<mainTaskPage> {
 
   @override
   void initState() {
-
     _getThingsOnStartup().then((value) async {
       print('Load done');
     });
     super.initState();
-
   }
 
   Future _getThingsOnStartup() async {
     var gradeModel = TasksModel();
-    listTask =await gradeModel.getAllTasks();
-    if(listTask.length!=0){
-      _lastInsertedId = listTask[listTask.length-1].id!;
+    listTask = await gradeModel.getAllTasks();
+    if (listTask.length != 0) {
+      _lastInsertedId = listTask[listTask.length - 1].id!;
       print(_lastInsertedId);
     }
-    setState(() {
-
-    });
+    setState(() {});
   }
-
 
   @override
   Widget build(BuildContext context) {
-    return
-      Scaffold(
-        appBar: AppBar(
-          title: Text("Task List Page",
-              style: TextStyle(color: Colors.white, fontSize: 25)),
-          actions: [
-            IconButton(onPressed: (){
-              _deleteTask();
-            },
-                icon: const Icon(Icons.delete))
-          ],
-        ),
-        body:
-        Center(
-            child: ListView.builder(
-              itemCount: listTask.length,
-              itemBuilder: (BuildContext context, int index){
-                print(listTask[index]);
-                return new ListTile(
-                  title: Text(listTask[index].name!),
-                  subtitle: Text(listTask[index].description! + ",  Total time set: "+ listTask[index].time!),
-                  onLongPress: (){
-                    _selectedIndex = index;
-                    selectedID = listTask[_selectedIndex].id;
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => TaskStart(taskList: listTask[_selectedIndex])));
-                  },
-                  onTap:(){
-                    _selectedIndex = index;
-                    selectedID = listTask[_selectedIndex].id;
-                  } ,
-                );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Task List Page",
+            style: TextStyle(color: Colors.white, fontSize: 25)),
+        actions: [
+          IconButton(
+              onPressed: () {
+                _deleteTask();
               },
-            )
-        ),
-
-        floatingActionButton: FloatingActionButton(
-          onPressed:_navigateToTaskAdd,
-          child: const Icon(Icons.add),
-        ),
-      );
+              icon: const Icon(Icons.delete)),
+          IconButton(
+              onPressed: () {
+                addAllTasksToFirestore(listTask);
+              },
+              icon: const Icon(Icons.car_crash))
+        ],
+      ),
+      body: Center(
+          child: ListView.builder(
+        itemCount: listTask.length,
+        itemBuilder: (BuildContext context, int index) {
+          print(listTask[index]);
+          return ListTile(
+            title: Text(listTask[index].name!),
+            subtitle: Text(listTask[index].description! +
+                ",  Total time set: " +
+                listTask[index].time!),
+            onLongPress: () {
+              _selectedIndex = index;
+              selectedID = listTask[_selectedIndex].id;
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) =>
+                      TaskStart(taskList: listTask[_selectedIndex])));
+            },
+            onTap: () {
+              _selectedIndex = index;
+              selectedID = listTask[_selectedIndex].id;
+            },
+          );
+        },
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _navigateToTaskAdd,
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 
   //This function call the TaskForm page to get the information required for the task.
   //After information is collected such as id and name, etc. the information is passed to a new function to add to the database later.
-  Future _navigateToTaskAdd() async{
+  Future _navigateToTaskAdd() async {
     print("Successfully clicked add");
-    List information =  (await Navigator.of(context).push(MaterialPageRoute(builder: (context) => TaskForm())) as List);
+    List information = (await Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => TaskForm())) as List);
     id_counter++;
     _addList(information);
   }
-
 
   //This onGoBack function is used to update the list and to set the lastInsertedID correctly for when the page is reset
   Future onGoBack(dynamic value) async {
@@ -109,10 +111,10 @@ class _mainTaskPageState extends State<mainTaskPage> {
     List grades1 = await gradeModel.getAllTasks();
 
     listTask = grades1;
-    if(listTask.length > 0){
+    if (listTask.length > 0) {
       _lastInsertedId = listTask[listTask.length - 1].id!;
-    }else{
-     _lastInsertedId = 0;
+    } else {
+      _lastInsertedId = 0;
     }
     setState(() {});
     return 0;
@@ -120,19 +122,31 @@ class _mainTaskPageState extends State<mainTaskPage> {
 
   //Here is the function for adding the task to the list.
   //The task is created with the information gathered and the add task from the model is called.
-  Future _addList(List info) async{
+  Future _addList(List info) async {
     if (_lastInsertedId == null) {
       _lastInsertedId = 0;
     }
-    Task task = Task(id: _lastInsertedId + 1, name: info[0], description: info[1], time: info[2], days: 0, complete: 0);
-    if (id_counter != null){
+    Task task = Task(
+        id: _lastInsertedId + 1,
+        name: info[0],
+        description: info[1],
+        time: info[2],
+        days: 0,
+        complete: 0);
+    if (id_counter != null) {
       task.id = id_counter;
     }
     _lastInsertedId = await _task.insertTask(task).then(onGoBack);
   }
 
   //Here is task delete task is run
-  void _deleteTask(){
+  void _deleteTask() {
     _task.deleteTodoWithId(selectedID).then(onGoBack);
+  }
+
+  // TODO: Delete function after testing
+  void addAllTasksToFirestore(List tasks) {
+    FirestoreService firestoreService = FirestoreService();
+    firestoreService.addTasks(tasks);
   }
 }
--- lib/storage/firestore_service.dart
+++ lib/storage/firestore_service.dart
@@ -13,7 +13,8 @@ class FirestoreService {
   }
 
   // Example: Create a reference to a document
-  DocumentReference<Map<String, dynamic>> getDocumentReference(String documentId) {
+  DocumentReference<Map<String, dynamic>> getDocumentReference(
+      String documentId) {
     return _firestore.collection('example_collection').doc(documentId);
   }
 
@@ -27,6 +28,42 @@ class FirestoreService {
       });
     } catch (e) {
       print('Error adding user: $e');
+    }
+  }
+
+  Future<void> addUsers(List<Map<String, dynamic>> users) async {
+    try {
+      final CollectionReference<Map<String, dynamic>> usersCollection =
+          _firestore.collection('users');
+
+      final List<Future<void>> futures = [];
+      for (final user in users) {
+        futures.add(usersCollection.add(user));
+      }
+
+      await Future.wait(futures);
+    } catch (e) {
+      print('Error adding users: $e');
+    }
+  }
+
+  // TODO: get rid of this function after using it as a reference.
+  // Tasks are not meant to be stored on cloud
+  // This function is meant to test connection between db and firestore
+  Future<void> addTasks(List tasks) async {
+    try {
+      final CollectionReference<Map<String, dynamic>> tasksCollection =
+          _firestore.collection('tasks');
+
+      final List<Future<void>> futures = [];
+      for (final task in tasks) {
+        futures.add(tasksCollection.add(task.toMap()));
+        print(task);
+      }
+
+      await Future.wait(futures);
+    } catch (e) {
+      print('Error adding tasks: $e');
     }
   }
 }
