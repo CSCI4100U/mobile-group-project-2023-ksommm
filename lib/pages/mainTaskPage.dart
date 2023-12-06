7eefcdeb8f6e893970dc4c49cee56d696a4e51c9
Keeran-Srikugan <keeran.srikugan@ontariotechu.net>
2023-12-06 12:42:45 -0500

    completing tasks

--- lib/pages/mainTaskPage.dart
+++ lib/pages/mainTaskPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:main/pages/taskStart.dart';
 
 import 'Task.dart';
 import 'TaskForm.dart';
@@ -71,12 +72,15 @@ class _mainTaskPageState extends State<mainTaskPage> {
                 return new ListTile(
                   title: Text(listTask[index].name!),
                   subtitle: Text(listTask[index].description! + ",  Total time set: "+ listTask[index].time!),
-                  onTap: (){
-                    setState(() {
-                      _selectedIndex = index;
-                      selectedID = listTask[_selectedIndex].id;
-                    });
+                  onLongPress: (){
+                    _selectedIndex = index;
+                    selectedID = listTask[_selectedIndex].id;
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => TaskStart(taskList: listTask[_selectedIndex])));
                   },
+                  onTap:(){
+                    _selectedIndex = index;
+                    selectedID = listTask[_selectedIndex].id;
+                  } ,
                 );
               },
             )
@@ -89,41 +93,46 @@ class _mainTaskPageState extends State<mainTaskPage> {
       );
   }
 
+  //This function call the TaskForm page to get the information required for the task.
+  //After information is collected such as id and name, etc. the information is passed to a new function to add to the database later.
   Future _navigateToTaskAdd() async{
     print("Successfully clicked add");
-    List information =  await Navigator.of(context).push(MaterialPageRoute(builder: (context) => TaskForm())) as List;
+    List information =  (await Navigator.of(context).push(MaterialPageRoute(builder: (context) => TaskForm())) as List);
     id_counter++;
     _addList(information);
   }
 
 
-
+  //This onGoBack function is used to update the list and to set the lastInsertedID correctly for when the page is reset
   Future onGoBack(dynamic value) async {
     var gradeModel = TasksModel();
     List grades1 = await gradeModel.getAllTasks();
 
     listTask = grades1;
-    _lastInsertedId = listTask[listTask.length - 1].id!;
-
+    if(listTask.length > 0){
+      _lastInsertedId = listTask[listTask.length - 1].id!;
+    }else{
+     _lastInsertedId = 0;
+    }
     setState(() {});
     return 0;
   }
 
+  //Here is the function for adding the task to the list.
+  //The task is created with the information gathered and the add task from the model is called.
   Future _addList(List info) async{
     if (_lastInsertedId == null) {
       _lastInsertedId = 0;
     }
-    Task task = Task(id: _lastInsertedId + 1, name: info[0], description: info[1], time: info[2]);
+    Task task = Task(id: _lastInsertedId + 1, name: info[0], description: info[1], time: info[2], days: 0, complete: 0);
     if (id_counter != null){
       task.id = id_counter;
     }
     _lastInsertedId = await _task.insertTask(task).then(onGoBack);
   }
 
+  //Here is task delete task is run
   void _deleteTask(){
-    setState(() {});
     _task.deleteTodoWithId(selectedID).then(onGoBack);
-    onGoBack;
-
   }
 }
