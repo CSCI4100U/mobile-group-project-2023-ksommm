3de8a9b191bb579f6cdad68109cfdd9d29b31db8
mmuidev <matthewmui22@gmail.com>
2023-12-07 15:30:57 -0500

    added furniture assets and furniture references file

---
+++ lib/storage/furniture_references.dart
@@ -0,0 +1,66 @@
+List defaultFurnitureList = [
+  {
+    'id': 1,
+    'name': 'blue sofa',
+    'location': 'right',
+    'selected': 0,
+  },
+  {
+    'id': 2,
+    'name': 'blue tall cupboard',
+    'location': 'right',
+    'selected': 0,
+  },
+  {
+    'id': 3,
+    'name': 'bookshelf',
+    'location': 'left',
+    'selected': 0,
+  },
+  {
+    'id': 4,
+    'name': 'closet',
+    'location': 'left',
+    'selected': 0,
+  },
+  {
+    'id': 5,
+    'name': 'cupboard',
+    'location': 'left',
+    'selected': 0,
+  },
+  {
+    'id': 6,
+    'name': 'fancy wooden chair',
+    'location': 'right',
+    'selected': 0,
+  },
+  {
+    'id': 7,
+    'name': 'red sofa',
+    'location': 'left',
+    'selected': 0,
+  },
+  {
+    'id': 8,
+    'name': 'sink',
+    'location': 'left',
+    'selected': 0,
+  },
+  {
+    'id': 9,
+    'name': 'small shelf',
+    'location': 'right',
+    'selected': 0,
+  },
+  {
+    'id': 10,
+    'name': 'wooden chair',
+    'location': 'left',
+    'selected': 0,
+  },
+];
+
+void checkIfDatabaseEmpty() {
+
+}
